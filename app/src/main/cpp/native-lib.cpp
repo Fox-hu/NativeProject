1fa6e4a9125715b1ee014e202a1a9c0d0016ed93
hujia <hujia@bilibili.com>
2023-06-27 17:13:11 +0800

    feature 新增c更改java字段示例

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -14,24 +14,30 @@ Java_com_fox_nativeproject_MainActivity_stringFromJNI(
     return env->NewStringUTF(hello.c_str());
 }
 
-//c获取java中的普通字段
+//c获取java中的普通字段 并修改
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_fox_nativeproject_MainActivity_filedFromJNI(JNIEnv *env, jobject thiz) {
     jclass clazz = env->FindClass(NATIVE_CLASS_MAIN_ACTIVITY);
     jfieldID id = env->GetFieldID(clazz, "filed", "Ljava/lang/String;");
+    //获取java的普通字段
     jobject str = env->GetObjectField(thiz, id);
-    return static_cast<jstring>(str);
+    jstring  name = env->NewStringUTF("C修改后的普通字段");
+    env->SetObjectField(thiz,id,name);
+    return name;
 }
 
-//c获取java中的静态字段
+//c获取java中的静态字段 并修改
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_fox_nativeproject_MainActivity_staticFiledFromJNI(JNIEnv *env, jobject thiz) {
     jclass clazz = env->FindClass(NATIVE_CLASS_MAIN_ACTIVITY);
     jfieldID id = env->GetStaticFieldID(clazz, "staticFiled", "Ljava/lang/String;");
+    //获取java的静态字段
     jobject str = env->GetStaticObjectField(clazz, id);
-    return static_cast<jstring>(str);
+    jstring  name = env->NewStringUTF("C修改后的静态字段");
+    env->SetStaticObjectField(clazz,id,name);
+    return name;
 }
 extern "C"
 JNIEXPORT void JNICALL
--- app/src/main/java/com/fox/nativeproject/MainActivity.kt
+++ app/src/main/java/com/fox/nativeproject/MainActivity.kt
@@ -24,18 +24,20 @@ class MainActivity : AppCompatActivity() {
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
 
-        binding.filed.text = filedFromJNI()
+        filedFromJNI()
+        binding.filed.text = filed
 
-        binding.staticFiled.text = staticFiledFromJNI()
+        staticFiledFromJNI()
+        binding.staticFiled.text = staticFiled
 
         javaToC()
 
         //动态注册
-        Toast.makeText(this,"${loadAdd(1f,2f)}",Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "${loadAdd(1f, 2f)}", Toast.LENGTH_LONG).show()
     }
 
-    fun normalMethod(){
-        Log.i(TAG,"normal method invoke")
+    fun normalMethod() {
+        Log.i(TAG, "normal method invoke")
     }
 
     /**
@@ -44,17 +46,18 @@ class MainActivity : AppCompatActivity() {
      */
     external fun stringFromJNI(): String
 
-    external fun filedFromJNI():String
+    external fun filedFromJNI(): String
 
-    external fun staticFiledFromJNI():String
+    external fun staticFiledFromJNI(): String
 
     //c调用java的普通方法和静态方法
     external fun javaToC()
 
-    external fun loadAdd(a:Float,b:Float):Float
+    external fun loadAdd(a: Float, b: Float): Float
 
     companion object {
         const val TAG = "MainActivity"
+
         // Used to load the 'nativeproject' library on application startup.
         var staticFiled = "我是静态变量"
 
@@ -64,8 +67,8 @@ class MainActivity : AppCompatActivity() {
 
         //这里一定要加这个注解 否则jni调用就会非常的麻烦
         @JvmStatic
-        fun staticMethod(){
-            Log.i(TAG,"static method invoke")
+        fun staticMethod() {
+            Log.i(TAG, "static method invoke")
         }
     }
 
